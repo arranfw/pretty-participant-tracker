ec7326d367e8ff4749284611dc93d7b31452adad
arranfw <arran@freshworks.io>
2022-09-23 07:48:52 -0600

    format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import { seedParticipants } from './seeds/participant.seed';
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
   await seedParticipants(1000);
@@ -8,10 +8,10 @@ async function main() {
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

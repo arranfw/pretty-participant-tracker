c1272b838e5fb48413ad95af9ffaaafb2e130055
arranfw <arran@freshworks.io>
2022-09-23 13:27:22 -0600

    add pagination util methods

--- app/util/pagination.ts
+++ app/util/pagination.ts
@@ -11,3 +11,39 @@ export const getPaginationQuery = ({ page, pageSize }: PaginationQueryOptions): 
 
   return `?${paginationSearchParams.toString()}`;
 };
+
+export const getSortValue = (sortObject: SortObject, sortKey: string): string | null => {
+  switch (sortObject[sortKey]) {
+    case 'asc':
+      return 'desc';
+    case 'desc':
+      return null;
+    default:
+      return 'asc';
+  }
+};
+
+export type SortObject = Record<string, string>;
+
+export const sortStringToSortObject = (sortString: string) => {
+  const sortItem = sortString.split(',');
+  const sortObject: Record<string, string> = {};
+
+  sortItem.forEach((sortItem) => {
+    const sortDirection = sortItem.charAt(0);
+    const sortKey = sortItem.substring(1);
+
+    switch (sortDirection) {
+      case '+':
+        sortObject[sortKey] = 'asc';
+        break;
+      case '-':
+        sortObject[sortKey] = 'desc';
+        break;
+      default:
+        throw new Error('Invalid sort string');
+    }
+  });
+
+  return sortObject;
+};
